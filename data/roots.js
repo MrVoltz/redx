5a5a7ab8a3a989e6dc84e0d1fe43f63ffa419e41
MrVoltz <uzivatelmp@gmail.com>
2023-10-07 22:46:20 +0200

    Added collection folders to roots

--- data/roots.js
+++ data/roots.js
@@ -1,4 +1,5 @@
 module.exports = [
 	"resrec:///G-Resonite/Inventory/Resonite Essentials",
-	// "resrec:///U-alex-derpy-fox/R-ecdccacf-4bd6-4310-a517-3a29fa6b40f2",
+	"resrec:///U-alex-derpy-fox/R-B095730F5F1D87196280AE46A5821839F13490FA1561B4515807B7F6EF844C91",
+	"resrec:///U-alex-derpy-fox/R-ecdccacf-4bd6-4310-a517-3a29fa6b40f2",
 ]
